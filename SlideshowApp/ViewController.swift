6a7fbc4795becee60f063534ce2fb54f589fa3fd
suzuki.r <getty2017@gmail.com>
2020-05-03 16:40:23 +0900

    再生と停止の基礎を実装

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
   @IBOutlet weak var firstView: UIImageView!
   
   var displayImageNumber:Int = 0
+  
+  @IBOutlet weak var resumeButtonElement: UIButton!
+
+  var timer: Timer!
 
   var imageNameArray = ["スライド1枚目", "スライド2枚目", "スライド3枚目"]
 
@@ -44,6 +48,26 @@ class ViewController: UIViewController {
   }
   
   @IBAction func resumeButton(_ sender: Any) {
+    if self.timer == nil {
+       self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+      resumeButtonElement.setTitle("停止", for: .normal)
+    } else {
+      resumeButtonElement.setTitle("再生", for: .normal)
+      self.timer.invalidate()
+      self.timer = nil
+    }
+  }
+  
+  // selector: #selector(updatetimer(_:)) で指定された関数
+  // timeInterval: 0.1, repeats: true で指定された通り、0.1秒毎に呼び出され続ける
+  @objc func updateTimer(_ timer: Timer) {
+    if displayImageNumber < 2 {
+      displayImageNumber += 1
+    } else {
+      displayImageNumber = 0
+    }
+
+    firstView.image = UIImage(named: imageNameArray[displayImageNumber])
   }
   
 }
