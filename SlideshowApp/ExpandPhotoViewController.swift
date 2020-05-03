03afc76ac5151027aa7e1963d54c25563ee97ec9
suzuki.r <getty2017@gmail.com>
2020-05-03 15:11:06 +0900

    戻ると進むの実装

---
+++ SlideshowApp/ExpandPhotoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExpandPhotoViewController.swift
+//  SlideshowApp
+//
+//  Created by 鈴木凌太 on 2020/05/03.
+//  Copyright © 2020 鈴木凌太. All rights reserved.
+//
+
+import UIKit
+
+class ExpandPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var firstView: UIImageView!
+  
+  var displayImageNumber:Int = 0
+
+  var imageNameArray = ["スライド1枚目", "スライド2枚目", "スライド3枚目"]
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    firstView.image = UIImage(named: imageNameArray[displayImageNumber])
   }
+  
+  @IBAction func returnButton(_ sender: Any) {
+    if displayImageNumber == 0 {
+      displayImageNumber = 2
+    } else {
+      displayImageNumber -= 1
+    }
 
+    firstView.image = UIImage(named: imageNameArray[displayImageNumber])
+  }
+  
+  @IBAction func nextButton(_ sender: Any) {
+    if displayImageNumber < 2 {
+      displayImageNumber += 1
+    } else {
+      displayImageNumber = 0
+    }
 
+    firstView.image = UIImage(named: imageNameArray[displayImageNumber])
+  }
+  
+  @IBAction func resumeButton(_ sender: Any) {
+  }
+  
 }
 
